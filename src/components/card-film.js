da704441c3f0bcfa4d7cc11cdeb0ea66e2674cd8
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-04-10 12:02:18 +0300

    создает карточки фильмов на основе моков

--- src/components/card-film.js
+++ src/components/card-film.js
@@ -1,20 +1,25 @@
-export const createCardFilmTemplate = () => {
+export const createCardFilmTemplate = (filmData) => {
+  const {title, rating, releaseDate, runtime, genres, smallPoster, description, comments, isAddedToView, isMarkAsWatched, isMarkAsFavorite} = filmData;
+
+  const filmYear = releaseDate.getFullYear();
+  const showingDescription = description.length > 140 ? `${description.substring(0, 140)}...` : description;
+
   return (
     `<article class="film-card">
-      <h3 class="film-card__title">The Dance of Life</h3>
-      <p class="film-card__rating">8.3</p>
+      <h3 class="film-card__title">${title}</h3>
+      <p class="film-card__rating">${rating}</p>
       <p class="film-card__info">
-        <span class="film-card__year">1929</span>
-        <span class="film-card__duration">1h 55m</span>
-        <span class="film-card__genre">Musical</span>
+        <span class="film-card__year">${filmYear}</span>
+        <span class="film-card__duration">${runtime}</span>
+        <span class="film-card__genre">${genres[0]}</span>
       </p>
-      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-      <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-      <a class="film-card__comments">5 comments</a>
+      <img src="./images/posters/${smallPoster}" alt="${title}" class="film-card__poster">
+      <p class="film-card__description">${showingDescription}</p>
+      <a class="film-card__comments">${comments.length} ${comments.length === 1 ? `comment` : `comments`}</a>
       <form class="film-card__controls">
-        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${isAddedToView ? `film-card__controls-item--active` : ``}">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched ${isMarkAsWatched ? `film-card__controls-item--active` : ``}">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite ${isMarkAsFavorite ? `film-card__controls-item--active` : ``}">Mark as favorite</button>
       </form>
     </article>`
   );
--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import {createAllFilmsListTemplate} from "./components/all-films-list.js";
 import {createFilmsTemplate} from "./components/films.js";
 import {createMenuAndStatsTemplate} from "./components/menu-and-stats.js";
 import {createUserRankTemplate} from "./components/user-rank.js";
+import {generateArrayFilms} from "./mocks/films.js";
 
 
-const ALL_FILMS_COUNT = 5;
+const ALL_FILMS_COUNT = 20;
 const EXTRA_FILMS_COUNT = 2;
 const EXTRA_CLASS_FILMS = [`Top rated`, `Most commented`];
+
+const arrayFilms = generateArrayFilms(ALL_FILMS_COUNT);
 
 
 const render = (container, template, place = `beforeend`) => {
@@ -34,10 +37,9 @@ render(filmsElement, createAllFilmsListTemplate());
 const allFilmsListElement = filmsElement.querySelector(`.films-list`);
 const allFilmsContainerElement = allFilmsListElement.querySelector(`.films-list__container`);
 
+arrayFilms.forEach((film) => render(allFilmsContainerElement, createCardFilmTemplate(film)));
 
-for (let i = 0; i < ALL_FILMS_COUNT; i++) {
-  render(allFilmsContainerElement, createCardFilmTemplate());
-}
+
 render(allFilmsListElement, createButtonShowMoreTemplate());
 
 EXTRA_CLASS_FILMS.forEach((item) => {
@@ -51,15 +53,20 @@ const topRatedFilmsContainerElement = topRatedFilmsListElement.querySelector(`.f
 const mostCommentedFilmsListElement = extraFilmsListElement[1];
 const mostCommentedFilmsContainerElement = mostCommentedFilmsListElement.querySelector(`.films-list__container`);
 
+const topRatedFilms = arrayFilms.slice()
+  .sort((max, min) => min.rating - max.rating);
 
 for (let i = 0; i < EXTRA_FILMS_COUNT; i++) {
-  render(topRatedFilmsContainerElement, createCardFilmTemplate());
+  render(topRatedFilmsContainerElement, createCardFilmTemplate(topRatedFilms[i]));
 }
+
+const mostCommentedFilms = arrayFilms.slice()
+  .sort((max, min) => min.comments.length - max.comments.length);
 
 
 for (let i = 0; i < EXTRA_FILMS_COUNT; i++) {
-  render(mostCommentedFilmsContainerElement, createCardFilmTemplate());
+  render(mostCommentedFilmsContainerElement, createCardFilmTemplate(mostCommentedFilms[i]));
 }
 
-render(document.body, createFilmDetailsPopupTemplate());
+// render(document.body, createFilmDetailsPopupTemplate());
 
